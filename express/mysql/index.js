9e785f1c3d25a5842e00f7fdf2ced2b275b2448c
ym <1577861862@qq.com>
2018-12-08 09:38:28 +0800

    连接数据库成功

---
+++ express/mysql/index.js
@@ -0,0 +1,35 @@
+var mysql = require("mysql");
+
+// 基本配置信息
+var config={
+    host:"localhost",
+    port:"3306",
+    user:"root",
+    password:"1214.*",
+    database:"week2"
+}
+
+// 创建连接池
+var connection = mysql.createPool(config);
+
+function query(sql,data){
+    return new Promise((resolve,reject)=>{
+        connection.getConnection((err,connect)=>{
+            connect.query(sql,data,(sqlerr,rows)=>{
+                if(sql.sqlerr){
+                    reject(sqlerr);
+                    return;
+                }
+                resolve(rows);
+                // 释放连接
+                connect.release();
+            })
+        })
+    })
+}
+
+//抛出模块
+
+module.exports = {
+    query
+}
---
+++ express/mysql/sql.js
@@ -0,0 +1,4 @@
+module.exports = {
+    "SELECT-COUNT":"select count(*) from shop",
+    "SELECT":"select * from shop"
+}
